5361b81a421e3f51768026b60bc0ff206815c966
Brem0827 <guswhd284@naver.com>
2023-03-09 12:12:32 +0900

    React Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import {Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Book from './chapter03/book';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to = "./chapter03/book">Book</Link>
       </header>
+        <Routes>
+            <Route exact path="./chapter03/book" component={Book}></Route>
+        </Routes>
     </div>
   );
 }
