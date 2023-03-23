3637cc31528625c908517b768fdbc8a9bf75f0b8
Brem0827 <guswhd284@naver.com>
2023-03-23 11:27:49 +0900

    fix book
    2023/03/23

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Link to = "./chapter03/book">Book</Link>
-        <Link to = "./chapter04/clock">Clock</Link>
+        <Link to = "./book">Book</Link>
+        <Link to = "./clock">Clock</Link>
       </header>
         <Routes>
-            <Route exact path="./chapter03/book" component={Book}></Route>
-            <Route exact path="./chapter04/clock" component={Clock}></Route>
+            <Route exact path="./book" component={Book}></Route>
+            <Route exact path="./clock" component={Clock}></Route>
         </Routes>
     </div>
   );
